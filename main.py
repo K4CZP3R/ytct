91c54d16c6368c07417bd67113941387a76dd2ed
Serewiś,Kacper K <k.serewis@student.fontys.nl>
2020-10-12 23:19:34 +0200

    Add CORS

--- main.py
+++ main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from routers import auth, search, videos
 from exceptions import google_oauth_exception, ytct_missing_data
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:4200",
+    "http://localhost:4200"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(
     auth.router,
     prefix="/auth",
