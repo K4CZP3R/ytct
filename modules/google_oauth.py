c591708bebf31e93a08a9c6b93ecbcffd12859ef
kacperserewis@outlook.com <kacperserewis@outlook.com>
2021-01-13 17:11:43 +0100

    update

--- modules/google_oauth.py
+++ modules/google_oauth.py
@@ -14,7 +14,9 @@ class GoogleOauth:
 
         authorization_url, state = flow.authorization_url(
             access_type='offline',
-            include_granted_scopes='true'
+            include_granted_scopes='true',
+            prompt='consent'
+
         )
         return authorization_url
 
